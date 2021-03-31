f5d87015c24d48ec8e9d42e473a0197aa72f4663
Erin Coates Mann <erin.mann2019@gmail.com>
2021-03-31 14:43:07 -0700

    still trying to get panel data

--- static/js/app.js
+++ static/js/app.js
@@ -93,23 +93,40 @@ function loadCharts(id) {
       Plotly.newPlot('bubble', bubbleData, layout);
 
       // build pannel data section
-      var selectedMetaData = (data.metadata.filter(obj => obj.id == id))[0];
-      console.log(selectedMetaData);
+      var selectedMetaData = (data.metadata.filter(obj => obj.id == id));
+      // console.log(selectedMetaData);
+
+      var panelData = Object.keys(selectedMetaData);
+      console.log(panelData);
       // metadata key
       // you could do a table, or maybe simpler is just append like "h5"
   
       //get reference to panel-body
-      var panelbody = d3.select("panel-body");
-      
-      selectedMetaData.forEach(function(updatePanel) {
-        console.log(updatePanel);
-        var row = panelbody.append("tr");
-        Object.entries(panelbody).forEach(function([key,value]) {
-          console.log(key,value);
-          var cell = row.append("td");
-          cell.text(value);
-        })
-      })
+      // var panelbody = d3.select(".panel-body");
+      // // console.log(panelbody);
+
+      // for (var key in selectedMetaData) {
+      //   var panelData = [];
+      //   // console.log(`${key}:${selectedMetaData[key]}`);
+      //   panelData.push(`${key}:${selectedMetaData[key]}`);
+      //   console.log(panelData);
+      // }
+
+      // panelArray = panelData.concat();
+      // console.log(panelArray);
+      // panelData.forEach(function(panelInfo) {
+      //   console.log(panelInfo);
+      //   var row = panelbody.append("tr");
+      //   console.log(row);
+        // Object.entries(panelInfo).forEach(function([key, value]) {
+        //   console.log(key, value);
+        //   // Append a cell to the row for each value
+        //   // in the weather report object
+        //   var cell = row.append("td");
+        //   cell.text(value);
+        // });
+      // });
+
       // for (var key in selectedMetaData) {
       //     console.log(`${key}:${selectedMetaData[key]}`);
       //     idData = key;
