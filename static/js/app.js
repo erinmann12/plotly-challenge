5b13e09457b6ceeacffea88cbc63a17a1d72a13f
Erin Coates Mann <erin.mann2019@gmail.com>
2021-03-31 21:05:11 -0700

    got panel data

--- static/js/app.js
+++ static/js/app.js
@@ -111,9 +111,10 @@ function getPanelData(id) {
     var selectedMetaData = (data.metadata.filter(obj => obj.id == id))[0];
     console.log(selectedMetaData);
 
-    //make selectedMetaData an array
-    var panelData = Object.entries(selectedMetaData);
-    console.log(panelData)
+    // //make selectedMetaData an array
+    // var panelData = Object.entries(selectedMetaData);
+    // console.log(panelData);
+
 
     //get reference to sample-metadata 
     var panelbody = d3.select("#sample-metadata");
@@ -121,49 +122,14 @@ function getPanelData(id) {
     //empty info table every time a new id is selected
     panelbody.html("");
 
-    //retrieve the demographic data for the id selected and append it
-    for (key in panelData) { 
-     console.log(key);
-      //panelbody.append("h5").text(`${key}:${selectedMetaData[key]} \n`);
-    }
+    //selected each key,value pair and append to h5 
+    Object.entries(selectedMetaData)
+      .forEach(([key,value]) => panelbody.append("h5").text(`${key}:${selectedMetaData[key]} \n`));
+    
   });
  }
-//  
-//  // console.log(selectedMetaData);
 
-//  var panelData = Object.entries(selectedMetaData);
-//  console.log(panelData);
-//  // metadata key
-//  // you could do a table, or maybe simpler is just append like "h5"
-
-//  
- // // console.log(panelbody);
-
- // panelArray = panelData.concat();
- // console.log(panelArray);
- // panelData.forEach(function(panelInfo) {
- //   console.log(panelInfo);
- //   var row = panelbody.append("tr");
- //   console.log(row);
- //   
- //     console.log(key, value);
- //     // Append a cell to the row for each value
- //     // in the weather report object
- //     var cell = row.append("td");
- //     cell.text(value);
- //   });
- // });
-
- // for (var key in selectedMetaData) {
- //     console.log(`${key}:${selectedMetaData[key]}`);
- //     idData = key;
- //   };
- // }
- // for (var key in selectedMetaData) {
- //   console.log(`${key}:${selectedMetaData[key]}`);
- //   var pannelData = d3.selectAll("#sample-metadata").innerText += key;
- // };
-
+ 
 
 // read in JSON as promise
 d3.json("samples.json").then((data)=>{
