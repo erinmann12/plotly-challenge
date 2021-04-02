635360c19f9bc71ccb9825b800244f525645b294
Erin Coates Mann <erin.mann2019@gmail.com>
2021-04-01 21:42:34 -0700

    added images

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,36 @@
 //create charts
 function loadCharts(id) {
+
   // initially 940
   console.log(id);
+
   // make data available in this function
   d3.json("samples.json").then((data)=>{
   
     // check if you have data
     console.log(data);
+
+    //retrieve selected data from dropdown
     var selectedData = data.samples.filter(obj => obj.id == id);
     // check that selected object has been isolated
     console.log(selectedData);
+
     // create variables to get object out of array
     var otuids = selectedData[0].otu_ids;
     var sampleValues = selectedData[0].sample_values;
     var otuLabels = selectedData[0].otu_labels;
-    console.log(otuids);
-    console.log(sampleValues);
-    console.log(otuLabels);
+    // console.log(otuids);
+    // console.log(sampleValues);
+    // console.log(otuLabels);
   
     // Slice the first 10 objects for plotting
     var slicedIds = otuids.slice(0, 10);
     var slicedSamples = sampleValues.slice(0,10);
     var slicedLabels = otuLabels.slice(0,10);
-    console.log(slicedIds);
-    console.log(slicedSamples);
-    console.log(slicedLabels);
+    // console.log(slicedIds);
+    // console.log(slicedSamples);
+    // console.log(slicedLabels);
+
     //reverse the objects
     var reversedIds = slicedIds.reverse();
     var reversedSamples = slicedSamples.reverse();
@@ -32,6 +38,7 @@ function loadCharts(id) {
     console.log(reversedIds);
     console.log(reversedSamples);
     console.log(reversedLabels);
+
     // Build bar chart
     var trace1 = {
         x: reversedSamples,
@@ -40,8 +47,10 @@ function loadCharts(id) {
         type: "bar",
         orientation: "h"
     };
+
     //data
     var barData = [trace1];
+
     //layout 
     var layout = {
         title: "Top 10 OTUs Found",
@@ -56,8 +65,10 @@ function loadCharts(id) {
         },
         barmode: 'stack'
     };
-    // Render the plot to the div tag with id "plot"
+
+    // Render the plot to the div tag with id "bar"
     Plotly.newPlot("bar", barData, layout);
+
     // build bubble chart
     var trace1 = {
         x: otuids,
@@ -82,7 +93,7 @@ function loadCharts(id) {
     Plotly.newPlot('bubble', bubbleData, layout);
 
     // *************************************
-    //Bonus: build guage chart
+    //Bonus: build gauge chart
     // ******************************** 
     //get metadata info
     var metadata = data.metadata;
@@ -92,13 +103,13 @@ function loadCharts(id) {
     var selectedMetaData = (data.metadata.filter(obj => obj.id == id))[0];
     console.log(selectedMetaData.wfreq)
 
+    // build gauge
     var gaugeData = [
       {
         domain: { x: [0, 1], y: [0, 1] },
         type: "indicator",
         mode: "gauge+number",
         value: parseFloat(selectedMetaData.wfreq),
-        //values: [0-1, 1-2, 2-3, 3-4, 4-5, 5-6, 6-7, 7-8, 8-9],
         title: { text: "Scrubs per Week", font: { size: 24 } },
         gauge: {
           axis: { range: [null, 9] },
@@ -127,25 +138,26 @@ function loadCharts(id) {
 // build pannel data section
 function getPanelData(id) {
 
-// make data available in this function
-d3.json("samples.json").then((data)=>{
-  //get metadata info
-  var metadata = data.metadata;
-  // console.log(metadata);
-  
-  // retrieve selectedMetaData
-  var selectedMetaData = (data.metadata.filter(obj => obj.id == id))[0];
-  // console.log(selectedMetaData);
-  //get reference to sample-metadata 
-  var panelbody = d3.select("#sample-metadata");
-  
-  //empty info table every time a new id is selected
-  panelbody.html("");
+  // make data available in this function
+  d3.json("samples.json").then((data)=>{
+    //get metadata info
+    var metadata = data.metadata;
+    // console.log(metadata);
+    
+    // retrieve selectedMetaData
+    var selectedMetaData = (data.metadata.filter(obj => obj.id == id))[0];
+    // console.log(selectedMetaData);
 
-  //selected each key,value pair and append to h5 
-  Object.entries(selectedMetaData)
-    .forEach(([key,value]) => panelbody.append("h5").text(`${key}: ${selectedMetaData[key]} \n`));
-});
+    //get reference to sample-metadata 
+    var panelbody = d3.select("#sample-metadata");
+    
+    //empty info table every time a new id is selected
+    panelbody.html("");
+
+    //selected each key,value pair and append to h5 
+    Object.entries(selectedMetaData)
+      .forEach(([key,value]) => panelbody.append("h5").text(`${key}: ${selectedMetaData[key]} \n`));
+  });
 }
 
 // read in JSON as promise
@@ -157,6 +169,8 @@ d3.json("samples.json").then((data)=>{
       dropDown.append("option").text(dataObj).property("value",dataObj);
   })
   var id = data.names[0];
+
+  //call functions
   loadCharts(id);
   getPanelData(id);
 })
